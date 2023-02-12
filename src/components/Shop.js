5372c930465948a4ac4fe7e86000796bf33ab705
Ugur Yilmaz <ugurbiyomek@gmail.com>
2023-02-12 22:16:36 +0300

    Set fetching fakestoreapi

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Shop() {
-  return <div>Shop</div>;
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    const data = await fetch('https://fakestoreapi.com/products');
+    const items = await data.json();
+    setItems(items);
+    console.log(items);
+  };
+
+  return (
+    <div>
+      {items.map((item) => (
+        <h1 key={item.id}>{item.title}</h1>
+      ))}
+    </div>
+  );
 }
