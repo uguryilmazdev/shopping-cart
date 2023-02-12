b331b63d57fddb56e41fa8f5a724d9cf4333f4f4
Ugur Yilmaz <ugurbiyomek@gmail.com>
2023-02-12 20:35:51 +0300

    Update Header style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import Contact from './components/Contact';
+import Cart from './components/Cart';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {}
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
 
 export default App;
