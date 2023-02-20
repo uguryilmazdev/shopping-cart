5e0dc2ec7420bf2fdf5c1b3a1828299e650b7e68
Ugur Yilmaz <ugurbiyomek@gmail.com>
2023-02-20 15:44:50 +0300

    Add increase-decrease functionality to cart item

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -9,7 +9,15 @@ export default function CartItem({ item }) {
       <div className="cart-item-info">
         <div className="cart-item-title">{item.title}</div>
         <div className="cart-item-price">${item.price}</div>
-        <div className="cart-item-quantity">{item.quantity}</div>
+        <div className="cart-item-quantity">
+          <button type="button" onClick={() => decreaseCartItemQuantity(item)}>
+            -
+          </button>
+          <div>{item.quantity}</div>
+          <button type="button" onClick={() => increaseCartItemQuantity(item)}>
+            +
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -25,4 +33,24 @@ export function getItemsFromStorage() {
   });
 
   return items;
+}
+
+function decreaseCartItemQuantity(item) {
+  const product = JSON.parse(localStorage.getItem(item.id));
+  product.quantity -= 1;
+
+  //check if item needs to be removed
+  if (product.quantity === 0) {
+    localStorage.removeItem(item.id);
+  } else {
+    localStorage.setItem(item.id, JSON.stringify(product));
+    window.dispatchEvent(new Event('storage'));
+  }
+}
+
+function increaseCartItemQuantity(item) {
+  const product = JSON.parse(localStorage.getItem(item.id));
+  product.quantity += 1;
+  localStorage.setItem(item.id, JSON.stringify(product));
+  window.dispatchEvent(new Event('storage'));
 }
