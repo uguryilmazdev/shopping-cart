cf486369b760f82531fbbedf942e876156916adb
Ugur Yilmaz <ugurbiyomek@gmail.com>
2023-02-23 03:47:23 +0300

    Update app colors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import '../styles/Home.css';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="Home">
+      <div className="home-text-container">
+        <div className="home-title">BEST ONLINE STORE EVER!</div>
+        <button type="button">Shop now</button>
+      </div>
+    </div>
+  );
 }
