bbb7d4ccbb7d43499720def037144bd2e3b4177a
Ugur Yilmaz <ugurbiyomek@gmail.com>
2023-02-23 03:54:21 +0300

    Update button link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../styles/Home.css';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <div className="Home">
       <div className="home-text-container">
         <div className="home-title">BEST ONLINE STORE EVER!</div>
-        <button type="button">Shop now</button>
+        <Link to="/shop">
+          <button type="button">Shop now</button>
+        </Link>
       </div>
     </div>
   );
